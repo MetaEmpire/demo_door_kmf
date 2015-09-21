c417ae58226c939bb0c1207d4ccc9f1547f49b8b
MetaEmpire <kfejeran@uw.edu>
2015-09-20 23:36:40 -0700

    started adding serial coms

--- python code/demodoor.py
+++ python code/demodoor.py
@@ -6,7 +6,8 @@ from cobs_serial import cobs_serial
 #from ctypes import c_ulong
 from collections import deque
 
-rssi_threshold_x10 = 100
+rssi_threshold = 7
+rssi_max = 9
 
 header = """
 ****************************
@@ -28,11 +29,22 @@ psuedo code:
 
 "update(list, value)" places the new value in the list, drops the oldest value, recalculates the average, writes the average to the dictonary, and if that dictonary entry is above open door threshold, fire open door routine.
 
-"opendoor()" behavior tbd, but there are 2 ways of doing it so far... 
+"opendoor()" behavior tbd, but there are 2 ways of doing it so far (blocking or threads)... 
 """
 
 def main():
 	print header
+	
+	# Open the serial port
+	# ************************** USER ENTERED PORT ***************************
+	port = raw_input("Enter your serial port (EX: COM14)\n>>")
+	cobs = cobs_serial(port, 115200, 0.1)
+	
+	# ************************** DEFAULT DEBUG PORT ***************************
+	#print "Defaulting port to COM17. Change script if neeeded"
+	#cobs = cobs_serial('COM17', 115200, 0.1)
+	
+	# *************************** END PORT CONFIG ****************************	
 	
 	print "Adding hardcoded ID's to whitelist..."
 	white1 = 0x0201041B # apparent ID taken from C# code that worked with "observer" code.
@@ -54,55 +66,23 @@ def main():
 	print id_map
 	
 	while(True):
-		new_value = int(raw_input())
+		new_value = int(raw_input()) #simulate getting a message over serial
 		update_map(id_map, 33621019, new_value)
 		print id_map
-		
+		retarray = cobs.block_and_return()
+		print repr(retarray)
 		#real version
-		#
-	
-	#function def update_map(id_map, id_to_update (33621019 for ex), new_value)
-	#new_value = 13 #new value comes in
-	#temp = id_map[33621019][2].popleft() #new value bumps out oldest val
-	#id_map[33621019][0] -= temp
-	#id_map[33621019][0] += new_value #adjust the running total
-	#id_map[33621019][2].append(new_value) #record new value
-	#if id_map[33621019][0] > rssi_threshold_x10: opendoor(id_to_update)
-	
+		#timeout read from cobs serial
+		#dissect ID / rssi from message, call update_map
 
-	
-	#id_rssi_map[7] = whitelist #seems to work fine?
-	#list for each ID...
-	#list of these lists?
-	#another map? key being the ID, the list being the value?
-	
-	'''
-	operations needed for this list
-		-pop oldest value
-		-push newest value
-		-use both of those values to adjust the running total (to then adjust the running average)
-	
-	summary of thoughts: use deque for the list
-	use 3 maps for all data structure needs
-	ID/running-average map
-	ID/deque map
-	ID/running-total map
-	
-	object?
-	
-	new notes, map of ID/tuples. each tuple is [deque of numbers, running total, running average]
-	can index through list like "for each_id in this_map: 
-									if each_id[2] > rssi_threshold.....
-	'''
 	
 def update_map(id_map, id_to_update, new_value, debug = False):
 	if not(id_to_update in id_map):
 		return
 	
-	if new_value > rssi_max or new_value < rssi_min:
+	if new_value > rssi_max:
 		if debug:
 			print("RSSI value recieved that was outside expected ranges. Ignoring.")
-		
 		return
 	
 	temp = id_map[id_to_update][2].popleft() #new value bumps out oldest val
@@ -110,11 +90,12 @@ def update_map(id_map, id_to_update, new_value, debug = False):
 	id_map[id_to_update][0] += new_value #adjust the running total
 	id_map[id_to_update][2].append(new_value) #record new value
 	
-	if id_map[id_to_update][0] > rssi_threshold_x10: open_door(id_to_update)		
+	if id_map[id_to_update][0] > rssi_threshold*10: open_door(id_to_update)		
 	
-def open_door(id_to_update):
+def open_door(id_to_update, debug = False):
 	#send cobs frame to open the door
-	print("Door opened for ID: " + repr(id_to_update))
+	if debug:
+		print("Door opened for ID: " + repr(id_to_update))
 
 	
 if __name__ == '__main__':
